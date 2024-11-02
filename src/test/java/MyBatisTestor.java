fd0edeb35fb66bd90650a7a5091a5bc6b6a176b1
Donkor <donkor@yeah.net>
2024-11-02 17:46:01 +0800

    MyBatisUtils,junit4单元测试框架,Logback日志输出,Druid数据库连接池

---
+++ src/test/java/MyBatisTestor.java
@@ -0,0 +1,19 @@
+import com.imooc.oa.utils.MyBatisUtils;
+import org.junit.Test;
+
+public class MyBatisTestor {
+    @Test
+    public void testCase1() {
+        String result = (String) MyBatisUtils.executeQuery(sqlSession -> {
+            String out = (String) sqlSession.selectOne("test.sample");
+            return out;
+        });
+        System.out.println(result);
+    }
+
+    @Test
+    public void testCase2() {
+        String result = (String) MyBatisUtils.executeQuery(sqlSession -> sqlSession.selectOne("test.sample"));
+        System.out.println(result);
+    }
+}
