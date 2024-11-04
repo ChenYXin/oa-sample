8083c26d279c80bdc2443a0fcc4bb66d547cd4bc
Donkor <donkor@yeah.net>
2024-11-04 16:39:23 +0800

    封装ResponseUtils工具类

--- src/main/java/com/imooc/oa/controller/LoginServlet.java
+++ src/main/java/com/imooc/oa/controller/LoginServlet.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.imooc.oa.entity.User;
 import com.imooc.oa.service.UserService;
+import com.imooc.oa.utils.ResponseUtils;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -24,31 +25,34 @@ public class LoginServlet extends HttpServlet {
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.setCharacterEncoding("UTF-8");
-        resp.setContentType("application/json; charset=utf-8");
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        response.setContentType("application/json; charset=utf-8");
         //接收用户输入
-        String username = req.getParameter("username");
-        String password = req.getParameter("password");
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
         //调用业务逻辑
-        Map result = new LinkedHashMap<>();
+//        Map result = new LinkedHashMap<>();
+        ResponseUtils resp = null;
         try {
             User user = userService.checkLogin(username, password);
             //处理结果编码，0代表处理成功，非0代表处理失败
-            result.put("code", "0");
-            result.put("message", "success");
-            Map data = new LinkedHashMap();
-            data.put("user", user);
-            result.put("data", data);
+//            result.put("code", "0");
+//            result.put("message", "success");
+//            Map data = new LinkedHashMap();
+//            data.put("user", user);
+//            result.put("data", data);
+            resp = new ResponseUtils().put("user", user);
         } catch (Exception e) {
             e.printStackTrace();
-            result.put("code", e.getClass().getSimpleName());
-            result.put("message", e.getMessage());
+//            result.put("code", e.getClass().getSimpleName());
+//            result.put("message", e.getMessage());
+            resp = new ResponseUtils(e.getClass().getSimpleName(), e.getMessage());
         }
         //返回json结果
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-        String json = objectMapper.writeValueAsString(result);
-        resp.getWriter().println(json);
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+//        String json = objectMapper.writeValueAsString(result);
+        response.getWriter().println(resp.toJsonString());
     }
 }
