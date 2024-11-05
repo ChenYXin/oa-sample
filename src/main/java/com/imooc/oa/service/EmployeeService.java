6aa505cf9aca244d80cc1c6e8b23a738a5d45195
Donkor <donkor@yeah.net>
2024-11-05 16:21:00 +0800

    开发请假申请Service层

--- src/main/java/com/imooc/oa/service/EmployeeService.java
+++ src/main/java/com/imooc/oa/service/EmployeeService.java
@@ -4,6 +4,10 @@ import com.imooc.oa.entity.Employee;
 import com.imooc.oa.mapper.EmployeeMapper;
 import com.imooc.oa.utils.MyBatisUtils;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class EmployeeService {
     public Employee selectById(Long employeeId) {
         Employee employee = (Employee) MyBatisUtils.executeQuery(sqlSession -> {
@@ -11,5 +15,32 @@ public class EmployeeService {
             return employeeMapper.selectById(employeeId);
         });
         return employee;
+    }
+
+    public Employee selectLeader(Long employeeId) {
+        Employee l = (Employee) MyBatisUtils.executeQuery(sqlSession -> {
+            EmployeeMapper employeeMapper = sqlSession.getMapper(EmployeeMapper.class);
+            Employee employee = employeeMapper.selectById(employeeId);
+            Map params = new HashMap<>();
+            Employee leader = null;
+            if (employee.getLevel() < 7) {
+                //查询部门经理
+                params.put("level", 7);
+                params.put("departmentId", employee.getDepartmentId());
+                List<Employee> employees = employeeMapper.selectByParams(params);
+                leader = employees.get(0);
+            } else if (employee.getLevel() == 7) {
+                //查询总经理
+                params.put("level", 8);
+                List<Employee> employees = employeeMapper.selectByParams(params);
+                leader = employees.get(0);
+            } else if (employee.getLevel() == 8) {
+                //返回自己
+                leader = employee;
+
+            }
+            return leader;
+        });
+        return l;
     }
 }
--- src/test/java/com/imooc/oa/mapper/EmployeeMapperTest.java
+++ src/test/java/com/imooc/oa/mapper/EmployeeMapperTest.java
@@ -4,15 +4,44 @@ import com.imooc.oa.entity.Employee;
 import com.imooc.oa.utils.MyBatisUtils;
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import static org.junit.Assert.*;
 
 public class EmployeeMapperTest {
 
     @Test
     public void selectById() {
-        Employee emp=(Employee) MyBatisUtils.executeQuery(sqlSession -> {
+        Employee emp = (Employee) MyBatisUtils.executeQuery(sqlSession -> {
             EmployeeMapper employeeMapper = sqlSession.getMapper(EmployeeMapper.class);
             Employee employee = employeeMapper.selectById(4l);
+            System.out.println(employee);
+            return employee;
+        });
+    }
+
+    @Test
+    public void selectByParams1() {
+        Map params = new HashMap<>();
+        params.put("level", 7);
+        params.put("departmentId",2);
+        MyBatisUtils.executeQuery(sqlSession -> {
+            EmployeeMapper employeeMapper = sqlSession.getMapper(EmployeeMapper.class);
+            List<Employee> employee = employeeMapper.selectByParams(params);
+            System.out.println(employee);
+            return employee;
+        });
+    }
+
+    @Test
+    public void selectByParams2() {
+        Map params = new HashMap<>();
+        params.put("level", 8);
+        MyBatisUtils.executeQuery(sqlSession -> {
+            EmployeeMapper employeeMapper = sqlSession.getMapper(EmployeeMapper.class);
+            List<Employee> employee = employeeMapper.selectByParams(params);
             System.out.println(employee);
             return employee;
         });
