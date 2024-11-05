2e8f8797b556a751ad7c16a52a9fc0bbaaf6b4ef
Donkor <donkor@yeah.net>
2024-11-05 22:36:41 +0800

    开发请假审批Service层

--- src/main/java/com/imooc/oa/mapper/ProcessFlowMapper.java
+++ src/main/java/com/imooc/oa/mapper/ProcessFlowMapper.java
@@ -2,10 +2,12 @@ package com.imooc.oa.mapper;
 
 import com.imooc.oa.entity.ProcessFlow;
 
+import java.util.List;
+
 public interface ProcessFlowMapper {
     public void insert(ProcessFlow processFlow);
 
     public void update(ProcessFlow processFlow);
 
-    public ProcessFlow selectByFormId(Long formId);
+    public List<ProcessFlow> selectByFormId(Long formId);
 }
